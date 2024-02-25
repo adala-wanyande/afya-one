b1c0bbf21c7fd7226e589f2724f2781a5c109c65
adala-wanyande <benardwanyande@gmail.com>
2024-02-25 10:35:04 +0300

    refactor: remove unnecesary files and packages

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { ChakraProvider } from '@chakra-ui/react'
 import App from './App';
-import { AuthContextProvider } from './Auth/Context/AuthContext'
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <AuthContextProvider>
             <App />
-      </AuthContextProvider>
-    </ChakraProvider>
   </React.StrictMode>
 );
