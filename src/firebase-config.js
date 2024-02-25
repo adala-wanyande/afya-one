80f66054728d92cc97f91de9e874c8f6b6429579
adala-wanyande <benardwanyande@gmail.com>
2024-02-25 09:51:14 +0300

    fix: clean up firebase config file

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -13,5 +9,4 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
