a23f4a4e497db7439cfe076b9ff7cfc317cd8bff
adala-wanyande <benardwanyande@gmail.com>
2024-02-25 10:56:37 +0300

    chore: initialize firestore in firebase config file and export db instance

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -14,3 +15,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 export { auth };
+export const db = getFirestore(app);
