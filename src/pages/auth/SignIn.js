5cf0231c8f1e202cb85e2fa73f419e85639b87ea
adala-wanyande <benardwanyande@gmail.com>
2024-02-27 16:33:12 +0300

    feat(navigation): implement navigation from sign in page to sign up page

--- src/pages/auth/SignIn.js
+++ src/pages/auth/SignIn.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import { useNavigate, Link } from 'react-router-dom'; 
 import { signIn } from '../../functions/auth';
 
 function SignInForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const navigate = useNavigate(); // Initialize useNavigate
+  const navigate = useNavigate(); 
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       await signIn(email, password);
-      navigate('/user'); // Redirect to /user route upon successful login
+      navigate('/user'); 
     } catch (error) {
       console.error('Failed to sign in:', error);
       // Handle login errors (e.g., display a message to the user)
@@ -19,23 +19,28 @@ function SignInForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
-      <button type="submit">Sign In</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Sign In</button>
+      </form>
+      <p>
+        Don't have an account? <Link to="/signup">Sign up here</Link>.
+      </p>
+    </div>
   );
 }
 
