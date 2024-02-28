3dd5e947e595243d4ec9a44ac9f863ce78bb292d
adala-wanyande <benardwanyande@gmail.com>
2024-02-28 10:57:23 +0300

    feat(errors): handle sign up error management

--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -1,25 +1,48 @@
 import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom'; // Import useNavigate
-import { signUp } from '../../functions/auth';
+import { useNavigate, Link } from 'react-router-dom';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../firebase-config'; // Ensure you import your Firebase auth instance
 
 function SignUpForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const navigate = useNavigate(); // Initialize useNavigate
+  const navigate = useNavigate();
+  const [error, setError] = useState(''); // Added error state
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setError(''); // Reset error message on new submission
     try {
-      await signUp(email, password);
+      await createUserWithEmailAndPassword(auth, email, password);
       navigate('/user/setup'); // Redirect to /user/setup route upon successful signup
     } catch (error) {
       console.error('Failed to sign up:', error);
-      // Optionally handle sign up errors, e.g., show an error message
+      handleSignUpError(error); // Handle sign up errors
+    }
+  };
+
+  const handleSignUpError = (error) => {
+    switch (error.code) {
+      case 'auth/email-already-in-use':
+        setError('The email address is already in use by another account.');
+        break;
+      case 'auth/invalid-email':
+        setError('The email address is not valid.');
+        break;
+      case 'auth/operation-not-allowed':
+        setError('Email/password accounts are not enabled. Contact support.');
+        break;
+      case 'auth/weak-password':
+        setError('The password is too weak. Password should have at least 6 characters.');
+        break;
+      default:
+        setError('Failed to sign up. Please try again later.');
     }
   };
 
   return (
     <div>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
       <form onSubmit={handleSubmit}>
         <input
           type="email"
