d623f3eeff4d424d0874aa1d8fd2cefebdada5e0
adala-wanyande <benardwanyande@gmail.com>
2024-03-01 11:12:27 +0300

    fix: adjust max width error

--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -81,7 +81,7 @@ function SignUpForm() {
               <input class="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50" id="password" type="password" value={password}
                 onChange={(e) => setPassword(e.target.value)}/>
             </div>
-            <div class="grid gap-2">
+            <div class="grid gap-2 max-w-60">
               {error && <ErrorAlert>{error}</ErrorAlert>}
             </div>
         </div>
