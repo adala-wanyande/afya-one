4a3099b41d881aae4fd5b7dfb64d92ecaa709dcc
adala-wanyande <benardwanyande@gmail.com>
2024-03-08 09:12:56 +0300

    fix: remove dependency array from useEffect hook in Navbar component

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -26,12 +26,12 @@ const NavBar = () => {
   return (
     <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <Link to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={logo} class="h-8" alt="Flowbite Logo" />
           <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             Afya One
           </span>
-        </a>
+        </Link>
         <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <NavigationMenu>
             <NavigationMenuList>
