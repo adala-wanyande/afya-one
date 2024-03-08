aa0f6fa270d4a259c19a35c3ff1bcc8aac236fa1
adala-wanyande <benardwanyande@gmail.com>
2024-03-08 09:32:38 +0300

    fix: adjust authentication pages with appropriate placeholder email

--- src/pages/auth/SignIn.js
+++ src/pages/auth/SignIn.js
@@ -35,7 +35,7 @@ function SignInForm() {
         <div class="p-6 pt-0 grid gap-4">
             <div class="grid gap-2">
               <label class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70" for="email">Email</label>
-              <input class="w-full flex h-9 rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50" id="email" placeholder="m@example.com" type="email" value={email} 
+              <input class="w-full flex h-9 rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50" id="email" placeholder="email@example.com" type="email" value={email} 
                 onChange={(e) => setEmail(e.target.value)}/>
             </div>
             <div class="grid gap-2">
