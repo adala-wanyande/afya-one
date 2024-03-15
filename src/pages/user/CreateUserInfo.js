fb1c90208f6e9a6f34cfb937b841e76949d17740
adala-wanyande <benardwanyande@gmail.com>
2024-03-15 14:59:16 +0300

    style(user): style user onboarding page

--- src/pages/user/CreateUserInfo.js
+++ src/pages/user/CreateUserInfo.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { auth, db } from "../../firebase-config";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
+import { DatePicker } from '../../components/forms/DatePicker';
 import LoadingButton from "../../components/buttons/LoadingButton";
 
 function CreateUserInfoForm() {
@@ -60,45 +61,80 @@ function CreateUserInfoForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="mb-8">
-      <input
-        type="text"
-        value={fullName}
-        onChange={(e) => setFullName(e.target.value)}
-        placeholder="Full Name"
-        required
-      />
-      <input
-        type="date"
-        value={dateOfBirth}
-        onChange={(e) => setDateOfBirth(e.target.value)}
-        required
-      />
-      <input
-        type="number"
-        value={startingWeight}
-        onChange={(e) => setStartingWeight(e.target.value)}
-        placeholder="Starting Weight"
-        required
-      />
-      <input
-        type="number"
-        value={height}
-        onChange={(e) => setHeight(e.target.value)}
-        placeholder="Height"
-        required
-      />
-      <button type="submit" hidden={isSubmitting}>
-        Submit
-      </button>
-      {isSubmitting ? (
-        <>
-          <LoadingButton></LoadingButton>
-        </>
-      ) : (
-        console.log()
-      )}
-    </form>
+    <div class="justify-center flex mt-16">
+      <div class="rounded-xl border bg-card text-card-foreground shadow w-[340px]">
+        <div class="flex flex-col p-6 space-y-1">
+          <h3 class="font-semibold tracking-tight text-2xl">
+            Welcome to Afya One!
+          </h3>
+          <p class="text-sm text-muted-foreground">Let's get to know you. Feel free to share. This is a safe space.</p>
+        </div>
+        <form onSubmit={handleSubmit} class="p-6 pt-0 grid gap-4">
+          <div class="grid gap-2">
+            <label class="text-sm font-medium leading-none" for="fullName">
+              What's your name?
+            </label>
+            <input
+              class="w-full flex h-9 rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
+              id="fullName"
+              type="text"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              required
+            />
+          </div>
+          <div class="grid gap-2">
+            <label class="text-sm font-medium leading-none" for="dateOfBirth">
+              When were you born?
+            </label>
+            <DatePicker
+              name="dateOfBirth"
+              value={dateOfBirth}
+              onChange={(e) => setDateOfBirth(e.target.value)}
+            />
+          </div>
+          <div class="grid gap-2">
+            <label
+              class="text-sm font-medium leading-none"
+              for="startingWeight"
+            >
+              What's your starting weight? (kg)
+            </label>
+            <input
+              class="w-full flex h-9 rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
+              id="startingWeight"
+              type="number"
+              value={startingWeight}
+              onChange={(e) => setStartingWeight(e.target.value)}
+              placeholder="Starting Weight"
+              required
+            />
+          </div>
+          <div class="grid gap-2">
+            <label class="text-sm font-medium leading-none" for="height">
+              What's your starting height? (cm)
+            </label>
+            <input
+              class="w-full flex h-9 rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
+              id="height"
+              type="number"
+              value={height}
+              onChange={(e) => setHeight(e.target.value)}
+              placeholder="Height"
+              required
+            />
+          </div>
+          <div class="flex items-center pt-4">
+            <button
+              type="submit"
+              class="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 w-full"
+            >
+              {isSubmitting ? "Submitting..." : "Submit"}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
