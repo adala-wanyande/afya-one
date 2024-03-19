8254568e45d9f0a9b1cce4c537dd5b5a6260891e
adala-wanyande <benardwanyande@gmail.com>
2024-03-19 20:53:01 +0300

    fix: make form consistent - exercise name

--- src/pages/workout/CreateWorkout.js
+++ src/pages/workout/CreateWorkout.js
@@ -130,7 +130,7 @@ function CreateWorkout() {
                 name="name"
                 value={workout.name}
                 onChange={(event) => handleWorkoutChange(index, event)}
-                placeholder="Workout Name"
+                placeholder="Exercise Name"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4"
                 required
               />
