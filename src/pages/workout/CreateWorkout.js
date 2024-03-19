a8bda1b8be6d2bee78d8ba762d0d5aa25396d084
adala-wanyande <benardwanyande@gmail.com>
2024-03-19 21:20:38 +0300

    fix: add placeholder text to training day form element

--- src/pages/workout/CreateWorkout.js
+++ src/pages/workout/CreateWorkout.js
@@ -115,7 +115,7 @@ function CreateWorkout() {
               name="bodyPart"
               value={bodyPart}
               onChange={(e) => setBodyPart(e.target.value)}
-              placeholder="Training Day"
+              placeholder="e.g Leg Day, Upper Body, Chest etc."
               className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4"
               required
             />
