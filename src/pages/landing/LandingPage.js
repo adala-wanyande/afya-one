2440e4b036399a3665753ebc2dff0214b9f1d8a2
Adala Wanyande <awanyande@gmail.com>
2024-06-27 01:56:39 +0300

    style(landing): implement new landing page

--- src/pages/landing/LandingPage.js
+++ src/pages/landing/LandingPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDumbbell, faChartLine, faWeight, faHistory, faAppleAlt, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 import logo from "../../assets/images/afya-one-logo.jpg";
+
 
 const LandingPage = () => {
   return (
@@ -17,84 +20,110 @@ const LandingPage = () => {
             </Link>
           </div>
           <nav>
-            <Link to="/signin" className="text-gray-800 hover:text-gray-300 px-3 py-2 rounded-md">
+            <Link to="/signin" className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md">
               Sign In
-            </Link>
-            <Link to="/signup" className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md">
-              Sign Up
             </Link>
           </nav>
         </div>
       </header>
 
+
       {/* Hero Section */}
-      <section className="bg-white text-white py-8">
-        <div className="container mx-auto px-4 text-center">
-          <h2 className="text-4xl font-bold mb-4 text-red-600">Welcome to Afya One 🏋️</h2>
-          <p className="text-lg mb-4 text-gray-800">
-            It might just be time for you to improve your life, wasteman! 🫵
-          </p>
-          <Link
-            to="/signup"
-            className="bg-red-500 hover:bg-red-600 text-white px-6 py-3 rounded-md inline-block"
-          >
-            Get Started
-          </Link>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-8 lg:px-12">
+          <a href="#" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
+            <span className="text-xs bg-red-600 rounded-full text-white px-4 py-1.5 mr-2">New</span> 
+            <span className="text-sm font-medium">Training split feature just added!</span>
+            <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+          </a>
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Change your life, wasteman!🫵</h1>
+          <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at Afya One, we focus on helping you unlock your full fitness potential with innovative tools and tracking features.</p>
+          <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+            <Link to="/signup" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-red-500 hover:bg-red-600 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
+              Get Started
+              <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
+              </svg>
+            </Link>
+          </div>
         </div>
       </section>
 
+      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+
+
       {/* Features Section */}
-      <section className="bg-white py-8">
-        <div className="container mx-auto px-4">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-            {/* Feature 1 */}
-            <div className="p-6 bg-white border border-red-500 rounded-lg shadow-md">
-              <div className="flex items-center mb-2">
-                <h3 className="text-xl font-semibold text-black">📋 Track Workouts</h3>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+          <div className="mb-8 lg:mb-16">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white text-center">
+              Designed for your fitness journey
+            </h2>
+            <div className='w-full flex justify-center'>
+            <p className="text-gray-500 sm:text-xl dark:text-gray-400 text-center lg:px-64">
+              At Afya One, we provide the tools and support you need to reach your fitness goals. Track your workouts, maintain streaks, monitor your weight, and log your nutrition effortlessly.
+            </p>
+            </div>
+          </div>
+          <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-red-100 lg:h-12 lg:w-12 dark:bg-red-900">
+                <FontAwesomeIcon icon={faDumbbell} className="text-red-600 lg:text-lg dark:text-red-300" />
               </div>
-              <p className="text-gray-700">
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Track Workouts</h3>
+              <p className="text-gray-500 dark:text-gray-400">
                 Keep a detailed log of your workouts and retrieve them anytime.
               </p>
             </div>
-            {/* Feature 2 */}
-            <div className="p-6 bg-white border border-red-500 rounded-md shadow-md">
-              <div className="flex items-center mb-2">
-                <h3 className="text-xl font-semibold text-black">📈 Workout Streaks</h3>
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-red-100 lg:h-12 lg:w-12 dark:bg-red-900">
+                <FontAwesomeIcon icon={faChartLine} className="text-red-600 lg:text-lg dark:text-red-300" />
               </div>
-              <p className="text-gray-700">
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Workout Streaks</h3>
+              <p className="text-gray-500 dark:text-gray-400">
                 Maintain a streak of workout commits with a GitHub-like graph.
               </p>
             </div>
-            {/* Feature 3 */}
-            <div className="p-6 bg-white border border-red-500 rounded-md shadow-md">
-              <div className="flex items-center mb-2">
-                <h3 className="text-xl font-semibold text-black">⚖️ Weight Tracking</h3>
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-red-100 lg:h-12 lg:w-12 dark:bg-red-900">
+                <FontAwesomeIcon icon={faWeight} className="text-red-600 lg:text-lg dark:text-red-300" />
               </div>
-              <p className="text-gray-700">
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Weight Tracking</h3>
+              <p className="text-gray-500 dark:text-gray-400">
                 Monitor your weight progress with an intuitive graph feature.
               </p>
             </div>
-            {/* Feature 4 */}
-            <div className="p-6 bg-white border border-red-500 rounded-md shadow-md">
-              <div className="flex items-center mb-2">
-                <h3 className="text-xl font-semibold text-black">📜 Workout Retrieval</h3>
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-red-100 lg:h-12 lg:w-12 dark:bg-red-900">
+                <FontAwesomeIcon icon={faHistory} className="text-red-600 lg:text-lg dark:text-red-300" />
               </div>
-              <p className="text-gray-700">
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Workout Retrieval</h3>
+              <p className="text-gray-500 dark:text-gray-400">
                 Retrieve your previous workouts to know exactly what you did.
               </p>
             </div>
-            {/* Feature 5 */}
-            <div className="p-6 bg-white border border-red-500 rounded-md shadow-md">
-              <div className="flex items-center mb-2">
-                <h3 className="text-xl font-semibold text-black">🍎 Nutritional Entries</h3>
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-red-100 lg:h-12 lg:w-12 dark:bg-red-900">
+                <FontAwesomeIcon icon={faAppleAlt} className="text-red-600 lg:text-lg dark:text-red-300" />
               </div>
-              <p className="text-gray-700">
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Nutritional Entries</h3>
+              <p className="text-gray-500 dark:text-gray-400">
                 Log your meals and track your daily eating habits.
+              </p>
+            </div>
+            <div>
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-red-100 lg:h-12 lg:w-12 dark:bg-red-900">
+                <FontAwesomeIcon icon={faCalendarAlt} className="text-red-600 lg:text-lg dark:text-red-300" />
+              </div>
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Predefined Training Splits</h3>
+              <p className="text-gray-500 dark:text-gray-400">
+                Create and manage training splits tailored to your fitness goals.
               </p>
             </div>
           </div>
         </div>
       </section>
+
 
       {/* CTA Section */}
       <section className="bg-red-500 py-20 text-white">
@@ -113,9 +142,71 @@ const LandingPage = () => {
       </section>
 
       {/* Footer Section */}
-      <footer className="bg-black text-white py-4">
-        <div className="container mx-auto text-center">
-          <p>&copy; 2024 Afya One. All rights reserved.</p>
+      <footer className="p-4 bg-white sm:p-6 dark:bg-gray-800">
+        <div className="mx-auto max-w-screen-xl">
+          <div className="md:flex md:justify-between">
+            <div className="mb-6 md:mb-0">
+              <a href="https://afya-one.com" className="flex items-center">
+                <img src={logo} className="mr-3 h-8" alt="Afya One Logo" />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Afya One</span>
+              </a>
+            </div>
+            <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+              <div>
+                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+                <ul className="text-gray-600 dark:text-gray-400">
+                  <li className="mb-4">
+                    <a href="https://afya-one.com" className="hover:underline">Afya One</a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+                <ul className="text-gray-600 dark:text-gray-400">
+                  <li className="mb-4">
+                    <a href="https://github.com/adala-wanyande" className="hover:underline">Github</a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+                <ul className="text-gray-600 dark:text-gray-400">
+                  <li className="mb-4">
+                    <a href="#" className="hover:underline">Privacy Policy</a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:underline">Terms &amp; Conditions</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">Built with ❤️ by Benard Adala Wanyande.</span>
+            <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+              <a href="https://github.com/adala-wanyande" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                  <path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" />
+                </svg>
+              </a>
+              <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                  <path fillRule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clipRule="evenodd" />
+                </svg>
+              </a>
+              <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                  <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                </svg>
+              </a>
+              <a href="https://github.com/adala-wanyande/afya-one" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                  <path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" />
+                </svg>
+              </a>
+            </div>
+          </div>
         </div>
       </footer>
     </div>
